f1ab9f4ac6bbcf5bf48cd93927372d842af2bd6d
Alexis Bériot <alexis.beriot@epfedu.fr>
2023-10-28 20:24:44 +0200

    add db requests

--- Projet/script.py
+++ Projet/script.py
@@ -4,7 +4,8 @@ from PIL import Image
 from qrcode.image.styledpil import StyledPilImage
 from qrcode.image.styles.moduledrawers.pil import RoundedModuleDrawer
 from qrcode.image.styles.colormasks import RadialGradiantColorMask
-
+import pandas as pd
+import matplotlib.pyplot as plt
 
 def QRCode_generator(name):
     qr = qrcode.QRCode(
@@ -44,3 +45,84 @@ def QRCode_generator_logo(name, logo):
     file = "./static/img/QR_CODE_" + name + "_FORUM_CARRIERE_2022.png"
     img.save(file)
     return file
+
+def printAllStudents(db_file, name):
+    df_survey = pd.read_excel(db_file)
+    students = []
+    time_start = []
+    time_end = []
+    for i in range(len(df_survey['Nom'])):
+        if df_survey["Nom de l'entreprise"][i]==name:
+            time_start.append(df_survey['Heure de début'][i])
+            time_end.append(df_survey['Heure de fin'][i])
+            students.append(df_survey["Nom"][i])
+    df_student = pd.DataFrame(time_start, index=None, columns=['Heure de début'])
+    df_student.insert(1, 'Heure de fin', time_end, True)
+    df_student.insert(2, "Nom de l'élève", students, True)
+    return df_student
+
+def printTopEnteprise(db_file):
+    df_survey = pd.read_excel(db_file)
+    all_enterprise = list(set(df_survey["Nom de l'entreprise"]))
+    nb_visits = []
+    for enterprise in all_enterprise:
+        nb_students = 0
+        for i in range(len(df_survey['Nom'])):
+            if enterprise == df_survey["Nom de l'entreprise"][i]:
+                nb_students += 1
+        nb_visits.append(nb_students)
+    df_top_enterprise = pd.DataFrame(all_enterprise, index=None, columns=['Entreprise'])
+    df_top_enterprise.insert(1, 'Nombre de visites', nb_visits)
+    df_top_enterprise = df_top_enterprise.sort_values(by='Nombre de visites', ascending=False)
+    df_top_enterprise = df_top_enterprise.reset_index(drop=True)
+    return df_top_enterprise
+
+def printAverage(db_file):
+    df_survey = pd.read_excel(db_file)
+    all_enterprise = list(set(df_survey["Nom de l'entreprise"]))
+    average = []
+    for enterprise in all_enterprise:
+        nb_students = 0
+        note = 0
+        for i in range(len(df_survey["Nom de l'entreprise"])):
+            if enterprise == df_survey["Nom de l'entreprise"][i]:
+                nb_students += 1
+                note += int(df_survey['Indice de satisfaction suite à votre rencontre entreprise'][i].split(' ', 2)[0])
+        average.append(note/nb_students)
+    df_average = pd.DataFrame(all_enterprise, index=None, columns=['Entreprise'])
+    df_average.insert(1, 'Moyenne appréciation', average, True)
+    df_average = df_average.sort_values(by='Moyenne appréciation', ascending=False)
+    df_average = df_average.reset_index(drop=True)
+    plt.figure(figsize=(12,6))
+    plt.bar(df_average['Entreprise'], df_average['Moyenne appréciation'])
+    plt.title("Moyenne des notes d’appréciation par entreprise")
+    plt.xlabel("Entreprise")
+    plt.ylabel("Moyenne des notes")
+    img = "./Projet/static/img/average_by_enterprise.png"
+    plt.savefig(img)
+    return img
+
+def printPrcFreq(db_file):    
+    df_survey = pd.read_excel(db_file)
+    all_enterprise = list(set(df_survey["Nom de l'entreprise"]))
+    all_visitors = list(set(df_survey['Nom']))
+    nb_visitors = len(all_visitors)
+    prc_freq = []
+    for enterprise in all_enterprise:
+        nb_visits = 0
+        for i in range(len(df_survey['Nom'])):
+            if enterprise == df_survey["Nom de l'entreprise"][i]:
+                nb_visits += 1
+        prc_freq.append(nb_visits/nb_visitors*100)
+    df_prc_freq = pd.DataFrame(all_enterprise, index=None, columns=['Entreprise'])
+    df_prc_freq.insert(1, 'Pourcentage de fréquentation', prc_freq, True)
+    df_prc_freq = df_prc_freq.sort_values(by='Pourcentage de fréquentation')
+    df_prc_freq = df_prc_freq.reset_index(drop=True)
+    plt.figure(figsize=(12,6))
+    plt.barh(df_prc_freq['Entreprise'], df_prc_freq['Pourcentage de fréquentation'])
+    plt.title("Pourcentage de fréquentation par entreprise")
+    plt.xlabel("Entreprise")
+    plt.ylabel("Pourcentage de fréquentation")
+    img = "./Projet/static/img/prcfreq_by_enterprise.png"
+    plt.savefig(img)
+    return img
--- Projet/web-page.py
+++ Projet/web-page.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
-from script import QRCode_generator, QRCode_generator_logo
+from script import QRCode_generator, QRCode_generator_logo, printAllStudents, printAverage, printPrcFreq, printTopEnteprise
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'D}hC~9a,KK2Jx_#5V=)L-4VGq8.rgpcz5/h8B?4J36T36ut'
@@ -27,7 +27,7 @@ def qrcode():
 def add_db():
     if request.method == "POST":
         excel_file = request.form['excel_file']
-        file = open(db_file, "a")
+        file = open(db_file, "w")
         file.write(excel_file)
         file.close()
         flash('Base de donnée ajoutée !', 'success')
@@ -39,5 +39,7 @@ def dashboard():
     excel_file = db.read()
     if excel_file == "":
         flash('Veuillez ajouter une base de données pour visualiser le dashboard.', 'warning')
-
+    if request.method == "POST":
+        img = printAverage(db)
+        return(render_template('dashboard.html'))
     return render_template('dashboard.html')
